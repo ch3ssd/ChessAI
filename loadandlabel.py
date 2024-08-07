99f343bb341029ab03f08c32e67e1992291a1774
dwang2 <ericwangdong@hotmail.com>
2024-08-06 22:52:30 -0400

    Fixed bugs of loading images.

--- loadandlabel.py
+++ loadandlabel.py
@@ -1,28 +1,35 @@
 import os
 import cv2
 import numpy
+import argparse
 
-def load_and_label_images(base_path):
+def main():
+    # Create ArgumentParser object
+    parser = argparse.ArgumentParser(description="Load images and labels from a specified file path and print them")
+
+    # Add arguments
+    parser.add_argument('path', type=str, help='Path of images')
+
+    # Parse the arguments
+    args = parser.parse_args()
+
     images = []
     labels = []
-    class_names = os.listdir(base_path)
+    image_names = os.listdir(args.path)
 
-    for label, class_name in enumerate(class_names):
-        class_dir = os.path.join(base_path,class_name)
-        if os.path.isdir(class_dir):
-            for filename in os.listdir(class_dir):
-                if filename.endswith(('.jpg','.jpeg','.png')):
-                    image_path = os.path.join(class_dir, filename)
-                    image = cv2.imread(image_path)
-                    if image is not None:
-                        image = image/255.0
-                        images.append(image)
-                        labels.append(label)
+    for label, image_name in enumerate(image_names):
+        image_path = os.path.join(args.path, image_name)
+        if image_path.endswith(('.jpg','.jpeg','.png')):
+           image = cv2.imread(image_path)
+           if image is not None:
+              image = image/255.0
+              images.append(image)
+              #TODO the label shall be a notation instead of a sequence num
+              labels.append(label)
 
-    return numpy.array(images), numpy.array(labels), class_names
-
+    return numpy.array(images), numpy.array(labels), image_names
 if __name__ == "__main__":
-    base_path = 'ChessPieces Images/Dataset'
-    images, labels, class_names = load_and_label_images(base_path)
-    print(f"Loaded {len(images)} from {len(class_names)} classes")
-    print(f"Class names {class_names}")
+    images, labels, image_names = main()
+    print(f"Loaded {len(images)} from {len(image_names)} images")
+    for image_name, label in zip(image_names, labels):
+        print(f"Image: {image_name}, Label: {label}")
