e5122f0100adfeaa3bc8aa02defb624de05574e3
dwang2 <ericwangdong@hotmail.com>
2024-08-26 23:02:41 -0400

    Added determine device func Added function checks the availability of mps(GPU) Decomposed the train_model into smaller functions with single responsibilities

--- train_model.py
+++ train_model.py
@@ -1,7 +1,13 @@
 from fastai.vision.all import *
-import matplotlib.pyplot as plt
 import os
+import torch
+from torch.utils.data import ConcatDataset
 
+# Determine the device (MPS/CPU)
+def determine_device():
+    device = 'cuda' if torch.cuda.is_available() else 'cpu' if torch.backends.mps.is_available() else 'cpu'
+    print(f"device = {device}")
+    return device
 
 def get_dir_path(dir_name):
     # Get the current file's directory (tests/test_some_module.py)
@@ -9,57 +15,91 @@ def get_dir_path(dir_name):
     dir_path = os.path.join(current_file_dir, '.', dir_name)
     return dir_path
 
+def create_dataloaders_from_image(image_folder, valid_pct=0.3):
+    """
+        Create a single DataLoaders object from multiple image folders.
 
-def load_model(model_dir, model_filename):
-    file_path = os.path.join(model_dir, model_filename)
+        :param image_folder: folder path containing images.
+        :param valid_pct: Percentage of data to use for validation.
+        :return: Combined DataLoaders object.
+        """
+
+    print(f"Loading images from '{image_folder}")
     dls = ImageDataLoaders.from_folder(
-        file_path,
-        valid_pct=0.3,
-        item_tfms=Resize(224),
-        batch_tfms=aug_transforms(
-            mult=2,
-            min_scale=0.8,
-            max_zoom=1.1,
-            flip_vert=False,
-            max_rotate=10,
-            max_warp=0.1,
-            p_lighting=0.3
-        ) + [Brightness(p=0.2, max_lighting=0.2),
-             Contrast(p=0.2, max_lighting=0.2),
-             Saturation(p=0.2, max_lighting=0.2),
-             ],
-        seed=42,
-        bs=16
-    )
-
-    learn = vision_learner(dls, resnet18, metrics=accuracy, wd=1e-3)
-
-    checkpoint = torch.load(model_dir / model_filename)
-    learn.model.load_state_dict(checkpoint['model_state_dict'])
-    learn.opt.load_state_dict(checkpoint['optimizer_state_dict'])
-
-    # Ensure the state of the recorder is set correctly
-    learn.recorder.epoch = checkpoint['epoch']
-    learn.recorder.losses = checkpoint['loss']
-
-    print(f"Model and optimizer states loaded from {model_dir / model_filename}")
-    return learn
+            image_folder,
+            valid_pct=valid_pct,
+            item_tfms=Resize(224),
+            batch_tfms=aug_transforms(
+                mult=2,
+                min_scale=0.8,
+                max_zoom=1.1,
+                flip_vert=False,
+                max_rotate=10,
+                max_warp=0.1,
+                p_lighting=0.3
+            ) + [Brightness(p=0.2, max_lighting=0.2),
+                Contrast(p=0.2, max_lighting=0.2),
+                Saturation(p=0.2, max_lighting=0.2),
+                ],
+            seed=42,
+            bs=16
+        )
+    return dls
 
 
-def main():
-    model_dir = get_dir_path('models')
-    model_filename = 'chess_piece_model.pth'
+def train_model(dls, model_file_path):
+    """
+        Train a model using the provided DataLoaders and save the model checkpoint.
 
-    learn = load_model(model_dir, model_filename)
-
+        :param dls: DataLoaders object.
+        :param model_file_path: Path to save the model checkpoint.
+        """
+    if not os.path.isfile(model_file_path):
+        learn = vision_learner(dls, resnet18, metrics=accuracy, wd=1e-3)
+    else:
+        learn = reload_model_from_checkpoint(model_file_path, dls)
+    # Training the model
+    learn.model.to(determine_device())
     learn.fit_one_cycle(6, slice(1e-6, 1e-4), cbs=[EarlyStoppingCallback(patience=4), ReduceLROnPlateau()])
-
+    #Save the model checkpoint
     torch.save({
         'model_state_dict': learn.model.state_dict(),
         'optimizer_state_dict': learn.opt.state_dict(),
         'epoch': learn.recorder.epoch,
         'loss': learn.recorder.losses,
-    }, model_dir / model_filename)
+    }, model_file_path)
+    print(f"Model trained and saved to {model_file_path}")
+
+
+def reload_model_from_checkpoint(model_file_path, dls):
+    """
+        Reload the model from a checkpoint.
+
+        :param model_file_path: Path to the model checkpoint file.
+        :param dls: DataLoaders object.
+        :return: Learner with restored model and optimizer states.
+        """
+    if not os.path.isfile(model_file_path):
+        raise FileNotFoundError(f"The path '{model_file_path}' does not exist")
+    checkpoint = torch.load(model_file_path)
+    learn = vision_learner(dls, resnet18, metrics=accuracy, wd=1e-3)
+    learn.model.load_state_dict(checkpoint['model_state_dict'])
+    learn.opt.load_state_dict(checkpoint['optimizer_state_dict'])
+    learn.recorder.epoch = checkpoint['epoch']
+    learn.recorder.losses = checkpoint['loss']
+    print(f"Model and optimizer states loaded from {model_file_path}")
+    # You can now use `learn` for further training or inference
+    return learn
+
+
+def main():
+    model_dir = get_dir_path('models')
+    model_file_path = os.path.join(model_dir, 'chess_piece_model.pth')
+    training_data_folder = get_dir_path('TrainingImages')
+    dls = create_dataloaders_from_image(training_data_folder)
+
+    #Train model
+    train_model(dls, model_file_path)
 
 
 if __name__ == '__main__':
